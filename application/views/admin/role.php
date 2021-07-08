c798b2dc1a380b1b1dfbb85f97d5bb9aec9628fd
Zulfi Rizkiawan <zulfi.rizkiawan.27@gmail.com>
2021-07-08 12:17:37 +0700

    role

--- application/views/admin/role.php
+++ application/views/admin/role.php
@@ -70,7 +70,7 @@
         </div>
     </div>
 </div>
-
+ 
 <div class="modal fade" id="deleteRole" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
